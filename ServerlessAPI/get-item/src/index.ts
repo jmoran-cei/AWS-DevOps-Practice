49c0dd8fa812e8753f2a1d0ac0483d5211a2b19e
Jon Moran <Jonathan.Moran@SignetJewelers.com>
2024-07-01 18:55:41 -0400

    lambda update

---
+++ ServerlessAPI/get-item/src/index.ts
@@ -0,0 +1,42 @@
+import * as AWS from 'aws-sdk';
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+export const handler = async (event: any): Promise<any> => {
+    try {
+        // Parse itemId
+        const itemId = event.pathParameters.itemId;
+
+        // DynamoDB params
+        const params: AWS.DynamoDB.DocumentClient.GetItemInput = {
+            TableName: 'SampleItems',
+            Key: { id: itemId },
+        };
+
+        // Retrieve item from DynamoDB
+        const result = await dynamoDB.get(params).promise();
+
+        // Check if the item exists
+        if (!result.Item) {
+            // Item doesn't exist
+            return {
+                statusCode: 404,
+                body: JSON.stringify({ message: 'Item not found' }),
+            };
+        }
+
+        // Success
+        return {
+            statusCode: 200,
+            body: JSON.stringify(result.Item),
+        };
+    } catch (error) {
+    
+        // Unexpected Error
+        console.error('Error retrieving item:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Error retrieving item' }),
+        };
+    }
+};
